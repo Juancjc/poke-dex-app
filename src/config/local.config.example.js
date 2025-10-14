9e31ec44c1c4e2d04c274ecdf400e2bbe64e1fb9
juancjc <juan.coelho@ac.gov.br>
2025-10-14 12:35:01 -0500

    adiciona configuração para uso da API externa

--- src/config/local.config.example.js
+++ src/config/local.config.example.js
@@ -1,1 +1,3 @@
+export const USE_POCKETBASE = true; // Altere para false para usar a API externa
 export const POCKETBASE_URL = "http://localhost:8090";
+export const EXTERNAL_API_URL = "url_da_api_externa_aqui"; // Altere para a URL da API externa se USE_POCKETBASE for false
