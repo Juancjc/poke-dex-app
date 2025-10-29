b0ff5d268b5da89a6612929a323ad6d991621d82
Juancjc <jcjustinianoc@hotmail.com>
2025-10-29 16:49:10 -0500

    adiciona URL da API pública do PokéAPI ao arquivo de configuração

--- src/config/local.config.example.js
+++ src/config/local.config.example.js
@@ -1,3 +1,4 @@
 export const USE_POCKETBASE = true; // Altere para false para usar a API externa
 export const POCKETBASE_URL = "http://localhost:8090";
 export const EXTERNAL_API_URL = "url_da_api_externa_aqui"; // Altere para a URL da API externa se USE_POCKETBASE for false
+export const POKEDEX_API_URL = "https://pokeapi.co/api/v2"; // URL da API pública do PokéAPI
