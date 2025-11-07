55b98621eaf2c10c8cf796ab15a39d2cf0c4ed99
Juancjc <jcjustinianoc@hotmail.com>
2025-11-07 18:03:57 -0500

    fix: corrige formatação do arquivo de configuração do app

--- app.config.js
+++ app.config.js
@@ -6,13 +6,13 @@ export default {
     slug: "poke-dex-app",
     version: "1.0.0",
     android: {
-      package: "com.juancjc.pokedex"
+      package: "com.juancjc.pokedex",
     },
     extra: {
       POCKETBASE_URL: POCKETBASE_URL,
       eas: {
-        projectId: "22b5f7d1-6cbd-430a-b35c-afa01f09595b"
-      }
+        projectId: "22b5f7d1-6cbd-430a-b35c-afa01f09595b",
+      },
     },
   },
 };
